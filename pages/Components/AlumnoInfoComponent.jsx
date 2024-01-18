4e8a0b5c1d42152dbb564ed32d490114c7ddc017
Luis Arques <luisarquescalero@gmail.com>
2024-01-18 14:27:03 +0100

    Creación del repo

---
+++ pages/Components/AlumnoInfoComponent.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+
+export default function AlumnoInfoComponent(props){
+    const {nombreAlumno, emailAlumno} = props
+
+
+    const [pagado, setPagado] = useState(false)
+
+    return (
+        <div></div>
+    )
+}
